6877b8e2d2bf9a945c5d73393ec3dc2aa325c235
ramiJoudi <rami.joudi1@esprit.tn>
2021-03-01 19:43:00 +0100

    first commit

---
+++ src/main/java/VORTEX/DARIPROJECT/DariProjectApplication.java
@@ -0,0 +1,13 @@
+package VORTEX.DARIPROJECT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DariProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DariProjectApplication.class, args);
+	}
+
+}
